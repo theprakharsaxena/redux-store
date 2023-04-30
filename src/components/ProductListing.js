49c2974e29be2d67294f0cfe96ff2ae034c740ca
theprakharsaxena <prakharsaxena5125@gmail.com>
2023-04-30 09:33:27 +0530

    effect

--- src/components/ProductListing.js
+++ src/components/ProductListing.js
@@ -6,16 +6,9 @@ import { setProducts } from "../redux/actions/productAction";
 const ProductListing = () => {
   const dispatch = useDispatch();
 
-  // const fetchAllProduct = async () => {
-  //   const response = await axios
-  //     .get("https://fakestoreapi.com/products")
-  //     .catch((err) => console.log(err));
-  //   dispatch(setProducts(response.data));
-  // };
-
   useEffect(() => {
     dispatch(setProducts());
-  });
+  },[]);
 
   return (
     <div>
